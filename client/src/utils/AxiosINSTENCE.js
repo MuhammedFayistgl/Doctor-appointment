d2e3a49a085db4d695b27c51aa4b43f9d9a90db0
muhammedfayisthangal@gmail.com <muhammedfayisthangal@gmail.com>
2024-02-08 13:09:48 -0500

    Update url to backend

--- client/src/utils/AxiosINSTENCE.js
+++ client/src/utils/AxiosINSTENCE.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const AxiosConnection = axios.create({
-  baseURL: 'https://doctorappointment-qjkq.onrender.com',
+  baseURL: 'https://doctor-dcql.onrender.com',
   // baseURL: 'https://cute-plum-angler-cuff.cyclic.app',
   // Producton url 
   // baseURL: 'http://localhost:5000/',
