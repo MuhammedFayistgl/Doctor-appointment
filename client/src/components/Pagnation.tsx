110d6d615b52167ae24ec46bff62bbc110347239
Muhammad Fayis Thangal <77223454+MuhammedFayistgl@users.noreply.github.com>
2024-03-09 20:55:05 -0500

    git conflict

--- client/src/components/Pagnation.tsx
+++ client/src/components/Pagnation.tsx
@@ -1,16 +1,16 @@
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
-
-// eslint-disable-next-line react/prop-types
-const Pagnation = ({length,setpage}) => {
-
-
-	return (
-		<Stack spacing={2}>
-			<Pagination count={length} variant="outlined" shape="rounded" onChange={(e,v)=> setpage(v)} />
-		</Stack>
-	);
+type PagnationProps = {
+    length: number;
+    setpage: (page: number) => void;
+};
+const Pagnation = ({ length, setpage }: PagnationProps) => {
+    return (
+        <Stack spacing={2}>
+            <Pagination count={length} variant="outlined" shape="rounded" onChange={(e, v) => setpage(v)} />
+        </Stack>
+    );
 };
 
 export default Pagnation;
