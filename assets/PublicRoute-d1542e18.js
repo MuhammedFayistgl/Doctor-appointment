508612d126e367369715228594a59c4a681d2ec1
Muhammad Fayis Thangal <77223454+MuhammedFayistgl@users.noreply.github.com>
2024-03-09 20:49:58 -0500

    Updates

--- assets/PublicRoute-d1542e18.js
+++
@@ -1,1 +0,0 @@
-import{G as s,y as o,N as r}from"./index-b2a666cf.js";const n=({children:e})=>{const[t]=s(["token"]);return t!=null&&t.token?e:o.jsx(r,{to:"/"})};export{n as default};
---
+++ assets/PublicRoute-d518f8bd.js
@@ -0,0 +1,1 @@
+import{t as s,l as o,N as r}from"./index-5c733ba6.js";const n=({children:e})=>{const[t]=s(["token"]);return t!=null&&t.token?o.jsx(r,{to:"/"}):e};export{n as default};
